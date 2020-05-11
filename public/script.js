1d689b582a9ba57e65a42275b31443e2906edef4
Glitch (hello-express) <none>
2020-05-11 17:18:10 +0000

    🍘🍂 Checkpoint ./server.js:15338946/3109 ./public/script.js:15338946/342

--- public/script.js
+++ public/script.js
@@ -1,15 +1,4 @@
 // client-side js, loaded by index.html
 // run by the browser each time the page is loaded
 
-console.log("hello world :o");
-/*
-const socket = new WebSocket('ws://glitch-wesocket-chat.glitch.me:8080');
- 
-socket.addEventListener('open', () => {
-  socket.send('Hello World!');
-});
- 
-socket.addEventListener('message', event => {
-  console.log(`Message from server: ${event.data}`);
-});
-*/
+var WebSocketClient = require('websocket').client;
--- server.js
+++ server.js
@@ -23,6 +23,27 @@ const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
-var wsServer = new WebSocketServer({
-    httpServer: listener
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({
+  port: 8080,
+  perMessageDeflate: {
+    zlibDeflateOptions: {
+      // See zlib defaults.
+      chunkSize: 1024,
+      memLevel: 7,
+      level: 3
+    },
+    zlibInflateOptions: {
+      chunkSize: 10 * 1024
+    },
+    // Other options settable:
+    clientNoContextTakeover: true, // Defaults to negotiated value.
+    serverNoContextTakeover: true, // Defaults to negotiated value.
+    serverMaxWindowBits: 10, // Defaults to negotiated value.
+    // Below options specified as default values.
+    concurrencyLimit: 10, // Limits zlib concurrency for perf.
+    threshold: 1024 // Size (in bytes) below which messages
+    // should not be compressed.
+  }
 });
